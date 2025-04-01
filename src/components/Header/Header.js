85672680519781caa4ba278ddfd96d9cd74d550f
Dylserker <levant.dylan@gmail.com>
2025-04-01 12:59:02 +0200

    Rajout de gif

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../../styles/Header.css';
+import leftGif from '../../assets/gif/Selena.gif';
+import rightGif from '../../assets/gif/Vargas.gif';
 
-const Header = () => {
+const Header = ({ className }) => {
     return (
-        <header className="App-header">
-            <h1>Mon Portfolio</h1>
+        <header className={`header ${className}`}>
+            <div className="header-content">
+                <div className="title-container">
+                    <img src={leftGif} alt="Gif gauche" className="title-gif" />
+                    <h1>Mon Portfolio</h1>
+                    <img src={rightGif} alt="Gif droite" className="title-gif" />
+                </div>
+            </div>
         </header>
     );
 };
