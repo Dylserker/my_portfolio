92e9c10a0739d0de531394659bc47515aded0b95
Dylserker <levant.dylan@gmail.com>
2025-10-01 11:42:44 +0200

    Changement en ts du portfolio

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../styles/Header.css';
 import leftGif from '../../assets/gif/Selena.gif';
 import rightGif from '../../assets/gif/Vargas.gif';
+import logo from '../../assets/images/React_Icon.png';
 
 type HeaderProps = {
     className?: string;
@@ -13,7 +14,15 @@ const Header: React.FC<HeaderProps> = ({ className = '' }) => {
             <div className="header-content">
                 <div className="title-container">
                     <img src={leftGif} alt="Gif gauche" className="title-gif" />
-                    <h1>Portfolio de Levant Dylan</h1>
+                    <nav className="nav">
+                        <img src={logo} alt="Logo" className="logo" />
+                        <ul className="nav-links">
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#projects">Projects</a></li>
+                            <li><a href="#skills">Skills</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </nav>
                     <img src={rightGif} alt="Gif droite" className="title-gif" />
                 </div>
             </div>
